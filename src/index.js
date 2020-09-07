2c3bb15243f50973f5f8a0b7b5f457a506964d0d
Ivan Blažević <ivan.blazevic97@gmail.com>
2020-09-07 09:26:52 +0200

    fixed individual appointment days/years fetching

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 "use strict";
 
 import '../src/index.scss';
-//import '@fortawesome/fontawesome-free/js/js.all';
 
 $(document).ready(() => {
+
+    let counter = 1;
+    let dropdownYears = $("#year1 #time-years");
+    let dropdownDays = $("#day1 #time-days");
+    const currentYear = (new Date()).getFullYear();
+
+    appendYears(dropdownYears, 2000, currentYear);
+    getDays(1, currentYear, dropdownDays);
 
     function daysInMonth(month, year) {
         return new Date(year, month, 0).getDate();
     }
 
-    function getDays() {
-        let month = $("#time-months option:selected").val();
-        let year = $("#time-years option:selected").val();
+    function getDays(month, year, dropdown) {
         let days = daysInMonth(month, year);
 
-        let dropdownDays = $("#time-days");
-        clearOptions(dropdownDays);
-
-        appendOptions(dropdownDays, 1, days);
+        clearOptions(dropdown);
+        appendOptions(dropdown, 1, days);
     }
 
     function appendOptions(selectElement, start, end) {
@@ -31,8 +34,9 @@ $(document).ready(() => {
 
             if (i == currentYear) {
                 selectElement.append("<option selected value="+ i +">"+ i +"</option>");
+            } else {
+                selectElement.append("<option value="+ i +">"+ i +"</option>");
             }
-            selectElement.append("<option value="+ i +">"+ i +"</option>");
         }
     }
 
@@ -42,12 +46,14 @@ $(document).ready(() => {
 
     function addAppointment(target) {
         counter++;
-        let appointmentHTML = '<li><div class="appointment-container" id="appointment'+ counter +'"><span id="closeIcon"><i class="fas fa-times fa-2x pointer"></i></span><h1>Termin #'+ counter +'</h1><div class="time-container"><div class="time-item"><h4>Godina</h4><select id="time-years"></select></div><div class="time-item"><h4>Mjesec</h4><select id="time-months"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select></div><div class="time-item"><h4 style="margin-left: -5px;">Dan</h4><select id="time-days"></select><span id="colon">:</span></div><div class="time-item" style="margin-left: -3px;"><h4>Početak</h4><select id="time-start"></select></div><div class="time-item"><h4>Završetak</h4><select id="time-end"></select></div></div></li>';
+        let appointmentHTML = '<li><div class="appointment-container" id="appointment'+ counter +'"><span id="closeIcon"><i class="fas fa-times fa-2x pointer"></i></span><h1>Termin #'+ counter +'</h1><div class="time-container"><div class="time-item" id="year'+ counter +'"><h4>Godina</h4><select id="time-years"></select></div><div class="time-item" id="month'+ counter +'"><h4>Mjesec</h4><select id="time-months"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select></div><div class="time-item" id="day'+ counter +'"><h4 style="margin-left: -5px;">Dan</h4><select id="time-days"></select><span id="colon">:</span></div><div class="time-item" id="start'+ counter +'" style="margin-left: -3px;"><h4>Početak</h4><select id="time-start"></select></div><div class="time-item" id="end'+ counter +'"><h4>Završetak</h4><select id="time-end"></select></div></div></li>';
 
         target.before(appointmentHTML);
 
-        let appointmentID = $("#appointment" + counter + " #time-years");
-        appendappendYearsOptions(appointmentID, 2000, currentYear);
+        let yearsDropdown = $("#year" + counter + " #time-years");
+        let daysDropdown = $("#day" + counter + " #time-days");
+        appendYears(yearsDropdown, 2000, currentYear);
+        getDays(1, 2020, daysDropdown);
     }
 
     function removeAppointment(target) {
@@ -63,25 +69,28 @@ $(document).ready(() => {
     });
 
     $(document).on('change', '#time-months', () => {
-        getDays();
-    })
+        let dropdown = $(event.target).parent().next().find("#time-days");
+        let month = $(event.target).val();
+        let year = $(event.target).parent().prev().find("#time-years").val();
+
+        getDays(month, year, dropdown);
+    });
 
     $(document).on('change', '#time-years', () => {
-        getDays();
-    })
+        let dropdown = $(event.target).parent().next().next().find("#time-days");
+        let month = $(event.target).parent().next().find("#time-months").val();
+        let year = $(event.target).val();
+
+        // console.log(dropdown, month, year);
+
+        getDays(month, year, dropdown);
+    });
 
     $(document).on('click', '#closeIcon', () => {
         let target = $(event.target).parent().parent();
 
         removeAppointment(target);
     });
-
-
-    let counter = 1;
-    let dropdownYears = $("#time-years");
-    const currentYear = (new Date()).getFullYear();
-
-    appendYears(dropdownYears, 2000, currentYear);
 
 });
 
